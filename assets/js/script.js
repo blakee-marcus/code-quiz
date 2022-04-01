0f41e77b6269bbc62c3eae19bc62a8e4ad1f25a3
bm <blake0marcus@gmail.com>
2022-04-01 01:26:37 -0700

    add style

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        question: 'Commonly used data types DO Not Include:',
+        answers: [
+            { text: '1. strings', correct: false },
+            { text: '2. boolean', correct: true},
+            { text: '3. alerts', correct: false},
+            { text: '4. numbers', correct: false}
+        ]
+    },
+    {
+        question: 'Commonly used data types DO Not Include:',
+        answers: [
+            { text: '1. strings', correct: false },
+            { text: '2. boolean', correct: true},
+            { text: '3. alerts', correct: false},
+            { text: '4. numbers', correct: false}
+        ]
+    },
+    {
+        question: 'Commonly used data types DO Not Include:',
+        answers: [
+            { text: '1. strings', correct: false },
+            { text: '2. boolean', correct: true},
+            { text: '3. alerts', correct: false},
+            { text: '4. numbers', correct: false}
+        ]
+    },
+    {
+        question: 'Commonly used data types DO Not Include:',
+        answers: [
+            { text: '1. strings', correct: false },
+            { text: '2. boolean', correct: true},
+            { text: '3. alerts', correct: false},
+            { text: '4. numbers', correct: false}
+        ]
+    },
+    {
+        question: 'Commonly used data types DO Not Include:',
+        answers: [
+            { text: '1. strings', correct: false },
+            { text: '2. boolean', correct: true},
+            { text: '3. alerts', correct: false},
+            { text: '4. numbers', correct: false}
+        ]
+    },
+]
